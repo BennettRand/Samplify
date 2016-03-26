a83f96e00e42d900fb6d3120a07788d1c6b52d86
Ben Rand <brand@solarcity.com>
2016-03-25 17:03:11 -0700

    ImperialDistance concept

--- measurements.py
+++ measurements.py
@@ -276,6 +276,78 @@ class Measure:
 					  unit, sig=self.sig)
 
 
+class ImperialDistance(object):
+	M_PER_YARD = 0.9144
+	def __init__(self, thous=0, inches=0, feet=0, yards=0, chains=0, furlongs=0,
+				 miles=0):
+		self._value = Measure(0, unit=BaseUnits.m)
+		self.yards = yards
+		self.feet += feet
+		self.inches += inches
+		self.thous += thous
+		self.chains += chains
+		self.furlongs += furlongs
+		self.miles += miles
+
+	def to_base_prefix(self):
+		return self._value.to_base_prefix()
+
+	@property
+	def yards(self):
+		return self._value.value / self.M_PER_YARD
+
+	@yards.setter
+	def yards(self, y):
+		self._value = Measure(y * self.M_PER_YARD, unit=BaseUnits.m)
+
+	@property
+	def feet(self):
+		return self.yards * 3.0
+
+	@feet.setter
+	def feet(self, f):
+		self.yards = f / 3.0
+
+	@property
+	def inches(self):
+		return self.feet * 12.0
+
+	@inches.setter
+	def inches(self, i):
+		self.feet = i / 12.0
+
+	@property
+	def thous(self):
+		return self.inches * 1000.0
+
+	@thous.setter
+	def thous(self, t):
+		self.inches = t / 1000.0
+
+	@property
+	def chains(self):
+		return self.yards / 22.0
+
+	@chains.setter
+	def chains(self, c):
+		self.yards = c * 22.0
+
+	@property
+	def furlongs(self):
+		return self.chains / 10.0
+
+	@furlongs.setter
+	def furlongs(self, f):
+		self.chains = f * 10.0
+
+	@property
+	def miles(self):
+		return self.furlongs / 8.0
+
+	@miles.setter
+	def miles(self, m):
+		self.furlongs = m * 8.0
+
 
 if __name__ == "__main__":
 	count = Measure(5)
@@ -297,16 +369,11 @@ if __name__ == "__main__":
 	frequency = 100 / time_
 	print "frequency", frequency
 
-	current = Measure(5, unit = BaseUnits.A)
-	voltage = Measure(240, unit = DerivedUnits.V)
-	pf = Measure(.98, unit = BaseUnits.PF)
-	power = current * voltage * pf
-	print power
-	model_dc_power()
-	current = Measure(5, unit = BaseUnits.A)
-	voltage = Measure(240, unit = DerivedUnits.V)
-	power = current * voltage
-	print power
-	print power * Measure(1, unit=BaseUnits.PF)
-	model_ac_power()
-	print power
+	yard = ImperialDistance(yards = 1)
+	print yard.thous
+	print yard.inches
+	print yard.feet
+	print yard.yards
+	print yard.chains
+	print yard.furlongs
+	print yard.miles

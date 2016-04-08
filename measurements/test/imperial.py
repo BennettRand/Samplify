bdf7035fcb6d63031bb4be231b64166a1f83e9d1
Ben Rand <brand@solarcity.com>
2016-04-08 16:59:34 -0700

    Imperial test re-org

--- measurements/test/imperial.py
+++ measurements/test/imperial.py
@@ -6,34 +6,31 @@ from measurements import imperial
 
 class ImperialTest(unittest.TestCase):
 	def setUp(self):
+		self.foot = imperial.ImperialDistance(feet = 1)
+		self.foot_in_cm = si.Measure(30.48, 'c',si.BaseUnits.m)
+		self.foot_in_inches = imperial.ImperialDistance(inches = 12)
+		self.foot_in_thous = imperial.ImperialDistance(thous = 12000)
+		self.yard_in_feet = imperial.ImperialDistance(feet = 3)
+		self.yard = imperial.ImperialDistance(yards = 1)
+		self.yards_in_mile = imperial.ImperialDistance(yards = 1760)
+		self.chains_in_mile = imperial.ImperialDistance(chains = 80)
+		self.furlongs_in_mile = imperial.ImperialDistance(furlongs = 8)
+		self.mile = imperial.ImperialDistance(miles = 1)
 		return
 
 	def test_metric_conversion(self):
-		foot = imperial.ImperialDistance(feet = 1)
-		foot_in_cm = si.Measure(30.48, 'c',si.BaseUnits.m)
-
-		self.assertEqual(foot_in_cm, foot)
-		self.assertEqual(foot_in_cm, foot.metric)
-		self.assertEqual(foot, foot_in_cm)
-		self.assertEqual(foot.metric, foot_in_cm)
+		self.assertEqual(self.foot_in_cm, self.foot)
+		self.assertEqual(self.foot_in_cm, self.foot.metric)
+		self.assertEqual(self.foot, self.foot_in_cm)
+		self.assertEqual(self.foot.metric, self.foot_in_cm)
 
 	def test_conversions(self):
-		foot = imperial.ImperialDistance(feet = 1)
-		foot_in_inches = imperial.ImperialDistance(inches = 12)
-		foot_in_thous = imperial.ImperialDistance(thous = 12000)
-		yard_in_feet = imperial.ImperialDistance(feet = 3)
-		yard = imperial.ImperialDistance(yards = 1)
-		yards_in_mile = imperial.ImperialDistance(yards = 1760)
-		chains_in_mile = imperial.ImperialDistance(chains = 80)
-		furlongs_in_mile = imperial.ImperialDistance(furlongs = 8)
-		mile = imperial.ImperialDistance(miles = 1)
-
-		self.assertEqual(foot_in_inches, foot)
-		self.assertEqual(foot_in_thous, foot)
-		self.assertEqual(yard_in_feet, yard)
-		self.assertEqual(yards_in_mile, mile)
-		self.assertEqual(chains_in_mile, mile)
-		self.assertEqual(furlongs_in_mile, mile)
+		self.assertEqual(self.foot_in_inches, self.foot)
+		self.assertEqual(self.foot_in_thous, self.foot)
+		self.assertEqual(self.yard_in_feet, self.yard)
+		self.assertEqual(self.yards_in_mile, self.mile)
+		self.assertEqual(self.chains_in_mile, self.mile)
+		self.assertEqual(self.furlongs_in_mile, self.mile)
 
 	def tearDown(self):
 		return

745276027757ee31bc1e2d52379cf6a2a82fca2f
Ben <compdude222@gmail.com>
2016-04-24 18:16:43 -0700

    Creating a setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name="samplify",
+	version="0.1.0",
+	url="http://github.com/BennettRand/Samplify/",
+	description="Take, save, and manipulate objects that store both maginitude and measurement information.",
+	author="Bennett Rand",
+	author_email="bennett.h.rand@gmail.com",
+	classifiers=[
+		"License :: OSI Approved :: MIT License",
+		"Topic :: Utilities",
+		"Operating System :: OS Independent"
+	],
+	packages=find_packages(),
+	install_requires=[],
+	license="MIT",
+	platforms=["Linux", "Win"]
+)

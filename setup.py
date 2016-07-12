d65caf71a420a8ab69c5ab2cf835abff85ed39de
Ben Rand <brand@solarcity.com>
2016-07-12 16:34:42 -0700

    Version++

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="samplify",
-	version="0.1.0",
+	version="0.1.1",
 	url="http://github.com/BennettRand/Samplify/",
 	description="Take, save, and manipulate objects that store both maginitude and measurement information.",
 	author="Bennett Rand",
